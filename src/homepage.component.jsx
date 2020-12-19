b3121d7b8d55e1ef3fea28e1352e9e70870ff3d8
KatjaSi <katja.sivertsen@gmail.com>
2020-12-19 13:54:33 +0100

    created react project and added files to homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className = 'homepage'>
+        <div className = 'categories-menu'>
+            <div className = 'category-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>Children</h1>
+                </div>
+            </div>
+            <div className = 'category-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>Wedding</h1>
+                </div>
+            </div>
+            <div className = 'category-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>Studio</h1>
+                </div>
+            </div>
+            <div className = 'category-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>Portrett</h1>
+                </div>
+            </div>
+            <div className = 'category-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>Posters</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
