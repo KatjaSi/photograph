5525a824219c9c7dce39f2b9943e2dcc71507b24
KatjaSi <katja.sivertsen@gmail.com>
2021-01-01 20:57:47 +0100

    Msonry galleries added

--- src/components/category-page/category-page.component.jsx
+++ src/components/category-page/category-page.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry";
 
 import Section_Data from '../categories-menu/data';
 import './category-page.styles.scss';
@@ -15,15 +16,18 @@ const CategoryPage = props => {
                     .map( ({...secProps}) => secProps.title)
                     } gallery</h1>
             </div> 
-            <div className = "gallery">
-            {
+            <ResponsiveMasonry
+                columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3,1200:4}}>
+                <Masonry gutter = {-1}>
+                {
                     Section_Data.filter(f => f.linkUrl === props.match.params.title ).map(({id, ...otherSectionProps}) => (
                         otherSectionProps.images.map( ({...secProps}) => (
                             <img src = {secProps.imageUrl} alt = {secProps.alt} />
-                        ))))};
+                        ))))}
                          
                     
-            </div>
+                </Masonry>
+            </ResponsiveMasonry>
         </div>
     );   
 };
